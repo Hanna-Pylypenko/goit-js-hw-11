34020f670d60357a307f5267f62fa96bee552c6f
Hanna-Pylypenko <pilipenkog679@gmail.com>
2022-06-04 23:48:06 +0300

    almost done

--- src/fetchPictures.js
+++ src/fetchPictures.js
@@ -1,9 +1,12 @@
 const BASE_URL = 'https://pixabay.com/api';
 const apiKey = '27847639-8e847d0d7182257a527cf2e5a';
 
-export function fetchPictures(searchItem) {
+export function fetchPictures(searchItem, pageNumber) {
+  if (searchItem === '') {
+    return;
+  }
   return fetch(
-    `${BASE_URL}/?key=${apiKey}&q=${searchItem}&image_type=photo&orientation=horizontal&safesearch=true&page=1&per_page=40`
+    `${BASE_URL}/?key=${apiKey}&q=${searchItem}&image_type=photo&orientation=horizontal&safesearch=true&page=${pageNumber}&per_page=40`
   ).then(res => {
     if (res.status === 404) {
       throw new Error();
--- src/index.js
+++ src/index.js
@@ -2,18 +2,54 @@ import { fetchPictures } from './fetchPictures';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import Notiflix from 'notiflix';
+import simpleLightbox from 'simplelightbox';
 
 const searchForm = document.querySelector('.search-form');
 const galleryContainer = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.load-more');
 searchForm.addEventListener('submit', onSubmitSearch);
+loadMoreBtn.addEventListener('click', onLoadMoreClick);
+
+let pageNumber = 1;
+let searchItem;
+let lightbox;
 
 function onSubmitSearch(e) {
+  pageNumber = 1;
   e.preventDefault();
-  const searchItem = e.currentTarget.elements.searchQuery.value;
-  //console.log(searchItem);
-  fetchPictures(searchItem)
+  searchItem = e.currentTarget.elements.searchQuery.value.trim();
+
+  fetchPictures(searchItem, pageNumber)
     .then(res => {
+      if (res.totalHits > 0) {
+        Notiflix.Notify.info(`Hooray! We found ${res.totalHits} images.`);
+      }
       cardsRender(res);
+      lightbox = new SimpleLightbox('.gallery a', {
+        captions: true,
+        captionsData: 'alt',
+        captionDelay: 250,
+      });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
+
+onLoadMoreClick();
+
+function onLoadMoreClick() {
+  pageNumber += 1;
+  fetchPictures(searchItem, pageNumber)
+    .then(res => {
+      if (res.hits.length === 0) {
+        loadMoreBtn.classList.remove('is-visible');
+        Notiflix.Notify.info(
+          "We're sorry, but you've reached the end of search results."
+        );
+      }
+      additionalCardsRender(res);
+      lightbox.refresh();
     })
     .catch(error => {
       console.log(error);
@@ -21,17 +57,51 @@ function onSubmitSearch(e) {
 }
 
 function cardsRender(res) {
-  if (res.totalHits === 0) {
+  console.log(res);
+  if (res.totalHits > 40) {
+    loadMoreBtn.classList.add('is-visible');
+  } else if (res.totalHits === 0) {
     Notiflix.Notify.failure(
       'Sorry, there are no images matching your search query. Please try again.'
     );
   }
+
   if (galleryContainer.innerHTML !== '') {
     galleryContainer.innerHTML = '';
   }
-  //console.log(res);
+
   const picsArray = res.hits;
-  //console.log(picsArray);
+
+  newGallery = picsArray
+    .map(
+      picture => `<div class="photo-card">
+    <a class="gallery__item" href="${picture.largeImageURL}">
+    <img src="${picture.webformatURL}" alt="${picture.tags}" loading="lazy" width="300px", height="200px"/>
+    </a>
+    <div class="info">
+    <p class="info-item">
+      <b>Likes </b>${picture.likes}
+     </p>
+    <p class="info-item">
+      <b>Views </b>${picture.views}
+    </p>
+    <p class="info-item">
+      <b>Comments </b>${picture.comments}
+    </p>
+    <p class="info-item">
+      <b>Downloads </b>${picture.downloads}
+    </p>
+    </div>
+    </div>`
+    )
+    .join('');
+
+  galleryContainer.innerHTML = newGallery;
+}
+
+function additionalCardsRender(res) {
+  const picsArray = res.hits;
+
   const newGallery = picsArray
     .map(
       picture => `<div class="photo-card">
@@ -55,6 +125,6 @@ function cardsRender(res) {
     </div>`
     )
     .join('');
-  //console.log(newGallery);
-  galleryContainer.innerHTML = newGallery;
+
+  galleryContainer.insertAdjacentHTML('beforeend', newGallery);
 }
